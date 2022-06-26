090fed7ba8705208f01b008810a630464107b8e8
Brenden Forward <44183550+bren0xa@users.noreply.github.com>
2022-06-26 15:39:29 -0400

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "preact";
+
+function App() {
+  return <div>Hello World</div>;
+}
+render(<App />, document.body)
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import preactPlugin from "@preact/preset-vite";
+
+export default defineConfig({
+  plugins: [preactPlugin()],
+});
