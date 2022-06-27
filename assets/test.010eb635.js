dfa146b612ca49d6fe7d65d30bcee5e4ef4b86ce
bren0xa <44183550+bren0xa@users.noreply.github.com>
2022-06-27 23:43:09 +0000

    Deploying to gh-pages from @ bren0xa/bforward.me@3391596a1b8c09c2b6bd74c12efe3c8bb8cbac37 🚀

---
+++ assets/test.010eb635.js
@@ -0,0 +1,14 @@
+const i={title:"Test Page",description:"Test description",tags:["foo","bar","baz"]},n=`<h1>Test</h1>
+<h2>This is a test document</h2>
+<p>This document is for testing</p>
+<ol>
+<li>One</li>
+<li>Two</li>
+<li>Three</li>
+</ol>
+<ul>
+<li>A</li>
+<li>B</li>
+<li>C</li>
+</ul>
+`;export{i as attributes,n as html};
