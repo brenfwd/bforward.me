2dee2d40a63c904d5a44e3ae4d0f7a27756089e1
bren0xa <bforward@umich.edu>
2022-12-23 17:14:13 -0500

    Fix 404

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,10 @@ import { render } from "preact";
 
 import { App } from "./App";
 
+const fromPath = window.localStorage.getItem("path");
+if (fromPath) {
+  window.localStorage.removeItem("path");
+  window.history.replaceState({}, "", fromPath);
+}
+
 render(<App />, document.getElementById("root")!);
